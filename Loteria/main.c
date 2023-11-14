4fab59df5b67a36470d10b54e44ddb2595ef34b9
Dvno02 <144128003+Dvno02@users.noreply.github.com>
2023-11-14 18:35:51 -0400

    Versión 2
    En esta segunda versión se mantiene lo de jugar 3 números al azar, mostrar el historial y salir de la lotería, además se le agrega nuevas opciones de juego las cuales son: jugar un número, jugar un palé y jugar una tripleta, en jugar un número se le permite al usuario jugar un número de su elección, en jugar un palé se le permite al usuario jugar dos números de su elección y en jugar una tripleta se le permite al usuario jugar 3 números de su elección.

--- Loteria/main.c
+++ Loteria/main.c
@@ -17,49 +17,108 @@ void mostrarHistorial(FILE *historialArchivo) {
     }
 }
 
+// Función para realizar una tirada y guardarla en el historial
+void realizarTirada(FILE *historialArchivo, int tipoJuego, int *numerosElegidos) {
+    int numero1, numero2, numero3;
+    time_t current_time;
+    time(&current_time);
+
+    switch (tipoJuego) {
+    case 1: // Tirar 3 números aleatorios
+        numero1 = generarNumeroAleatorio(0, 99);
+        numero2 = generarNumeroAleatorio(0, 99);
+        numero3 = generarNumeroAleatorio(0, 99);
+        break;
+    case 2: // Jugar un número
+        numero1 = generarNumeroAleatorio(0, 99);
+        numero2 = generarNumeroAleatorio(0, 99);
+        numero3 = generarNumeroAleatorio(0, 99);
+        break;
+    case 3: // Jugar un pale
+        numero1 = generarNumeroAleatorio(0, 99);
+        numero2 = generarNumeroAleatorio(0, 99);
+        numero3 = generarNumeroAleatorio(0, 99);
+        break;
+    case 4: // Jugar una tripleta
+        numero1 = generarNumeroAleatorio(0, 99);
+        numero2 = generarNumeroAleatorio(0, 99);
+        numero3 = generarNumeroAleatorio(0, 99);
+        break;
+    default:
+        printf("Opción de juego no válida.\n");
+        return;
+    }
+
+    fprintf(historialArchivo, "Tirada: %d %d %d - Fecha y hora: %s", numero1, numero2, numero3, ctime(&current_time));
+
+    // Imprimir en consola los resultados
+    if (tipoJuego == 1) {
+        printf("Tirada: %d %d %d\n", numero1, numero2, numero3);
+    } else {
+        printf("Tus números: %d %d %d - Números de la Lotería: %d %d %d\n", numerosElegidos[0], numerosElegidos[1], numerosElegidos[2], numero1, numero2, numero3);
+    }
+}
+
 int main() {
     srand(time(NULL));
 
     char opcion;
-    int numero1, numero2, numero3;  // Declarar las variables fuera del switch
+    int tipoJuego, numerosElegidos[3];
 
-    FILE *historialArchivo = fopen("historial.txt", "a+"); // Abierto para lectura y escritura, creando el archivo si no existe
+    FILE *historialArchivo = fopen("historial2.txt", "a+"); // Abierto para lectura y escritura, creando el archivo si no existe
 
     if (historialArchivo == NULL) {
-        printf("Error al abrir el archivo para guardar el historial.\n");
+        perror("Error al abrir el archivo para guardar el historial");
         return 1;
     }
 
     do {
-        printf("Bienvenido a la Lotería MJW\n");
+        printf("\nBienvenido a la Lotería MJW\n");
         printf("1. Tirar 3 números aleatorios\n");
-        printf("2. Mostrar historial de tiradas\n");
-        printf("3. Salir\n");
+        printf("2. Jugar un número\n");
+        printf("3. Jugar un pale\n");
+        printf("4. Jugar una tripleta\n");
+        printf("5. Mostrar historial de tiradas\n");
+        printf("6. Salir\n");
         printf("Elija una opción: ");
         scanf(" %c", &opcion);
 
         switch (opcion) {
-            case '1':
-                numero1 = generarNumeroAleatorio(0, 99);
-                numero2 = generarNumeroAleatorio(0, 99);
-                numero3 = generarNumeroAleatorio(0, 99);
-                time_t current_time;
-                time(&current_time);
-
-                fprintf(historialArchivo, "Tirada: %d %d %d - Fecha y hora: %s", numero1, numero2, numero3, ctime(&current_time));
-                printf("Tirada: %d %d %d\n", numero1, numero2, numero3);
-                break;
-            case '2':
-                mostrarHistorial(historialArchivo);
-                break;
-            case '3':
-                printf("Saliendo del programa. Cerrando el archivo...\n");
-                fclose(historialArchivo);
-                break;
-            default:
-                printf("Opción no válida. Inténtelo de nuevo.\n");
+        case '1':
+            realizarTirada(historialArchivo, 1, NULL); // Tipo de juego 1: Tirar 3 números aleatorios, númerosElegidos no se utiliza
+            break;
+        case '2':
+            printf("Ingrese el número a jugar: ");
+            scanf("%d", &numerosElegidos[0]);
+            realizarTirada(historialArchivo, 2, numerosElegidos); // Tipo de juego 2: Jugar un número
+            break;
+        case '3':
+            printf("Ingrese el primer número del pale: ");
+            scanf("%d", &numerosElegidos[0]);
+            printf("Ingrese el segundo número del pale: ");
+            scanf("%d", &numerosElegidos[1]);
+            realizarTirada(historialArchivo, 3, numerosElegidos); // Tipo de juego 3: Jugar un pale
+            break;
+        case '4':
+            printf("Ingrese el primer número de la tripleta: ");
+            scanf("%d", &numerosElegidos[0]);
+            printf("Ingrese el segundo número de la tripleta: ");
+            scanf("%d", &numerosElegidos[1]);
+            printf("Ingrese el tercer número de la tripleta: ");
+            scanf("%d", &numerosElegidos[2]);
+            realizarTirada(historialArchivo, 4, numerosElegidos); // Tipo de juego 4: Jugar una tripleta
+            break;
+        case '5':
+            mostrarHistorial(historialArchivo);
+            break;
+        case '6':
+            printf("Saliendo del programa. Cerrando el archivo...\n");
+            fclose(historialArchivo);
+            break;
+        default:
+            printf("Opción no válida. Inténtelo de nuevo.\n");
         }
-    } while (opcion != '3');
+    } while (opcion != '6');
 
     return 0;
 }
